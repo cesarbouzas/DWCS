bcd47e46fc0777a3d39ca4e2ad18d191c52801e1
cesar <cesarbouzaspaw@gmail.com>
2022-01-23 21:32:16 +0100

    tabalas creadas

--- Tema3_Ejercicios/control_Almacen.php
+++ Tema3_Ejercicios/control_Almacen.php
@@ -1,52 +1,59 @@
 <?php
 $con_bd=@mysqli_connect('localhost','admin','21212121');
 if(!$con_bd){
-    echo('Erro numero '.mysqli_connect_errno.'al establecerla conexion'.mysqli_connect_error().'.<br/>');
+    echo('Error número '.mysqli_connect_errno.'al establecer la conexión'.mysqli_connect_error().'/n');
 
 }else{
-    echo("conexion establecida con exito <br/>");
+    echo("Conexion establecida con exito .\n");
     
 }
 // creaccion de una base de datos
 if(mysqli_query($con_bd,'CREATE DATABASE almacenes')===TRUE){
-    echo('Se creo correctamente a BD llamada alamacenes.</br>');
+    echo("Se creo correctamente a BD llamada alamacenes.\n");
 }else{
 
-    echo('Error en la creaccion de la BD :'. mysqli_error($con_bd).'<br/>');
+    echo('Error en la creacción de la BD :'. mysqli_error($con_bd)."\n");
+}
+
+if(mysqli_query($con_bd,'USE almacenes')===TRUE){
+    echo("Se cambió la base actual a Alamacenes .\n");
+}else{
+
+    echo('Error en el cambio de bases de datos activa :'. mysqli_error($con_bd)."\n");
 }
 
 
 $muchotema="CREATE TABLE almacen (codigo int(4) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
                                     lugar varchar(100),
-                                    capacidad int(5));";
+                                    capacidad int(5))";
 
 
 if(mysqli_query($con_bd,$muchotema)===TRUE){
-    echo('Se creo correctamente las tablas.</br>');
+    echo("Se creo correctamente las tablas.\n");
 }else{
 
-    echo('Error en la creaccion de la tabla'. mysqli_error($con_bd).'<br/>');
+    echo('Error en la creaccion de la tabla '. mysqli_error($con_bd)."\n");
 }
 
 
 
 
 
-$muchotema="CREATE TABLE  caja (numreferencia char(5) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+$muchotema="CREATE TABLE  caja (numreferencia int(5) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
                                    contenido varchar(100),
                                     valor int(5),
                                     almacen int(4) UNSIGNED,
-                                    FOREIGN KEY(almacen) REFERENCES almacen(codigo) 
-                                                            ON DELETE NOTACTION,
-                                                            ON UPDATE CASCADE, 
+                                    FOREIGN KEY(almacen) REFERENCES almacen(codigo)
+                                                            ON DELETE RESTRICT
+                                                            ON UPDATE CASCADE
                                     )";
 
 
 if(mysqli_query($con_bd,$muchotema)===TRUE){
-    echo('Se creo correctamente las tablas.</br>');
+    echo("Se creo correctamente las tablas.\n");
 }else{
 
-    echo('Error en la creaccion de la tabla'. mysqli_error($con_bd).'<br/>');
+    echo('Error en la creaccion de la tabla :'. mysqli_error($con_bd)."\n");
 }
 
 
