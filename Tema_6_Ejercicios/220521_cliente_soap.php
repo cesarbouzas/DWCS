2239cf36e8bfefd364a1dbfc791c811c542de7ef
cesarbouzas <cesarbouzaspaw@gmail.com>
2022-05-21 17:05:18 +0200

    SERVICO

---
+++ Tema_6_Ejercicios/220521_cliente_soap.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <pre>
+<?php
+$servicio=new SoapClient("htttps:////cvnet.cpd.ua.es/servicioweb/publicos/pub_gestdocente.asmx?wsdl");
+$parametros=["es","2019"];
+$titulos=$servicio->wstitulosuni($parametros);
+var_dump($parametros);
+var_dump($titulos);
+
+?>
+</pre>
+</body>
+</html>
+<?php
